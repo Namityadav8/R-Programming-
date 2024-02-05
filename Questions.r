6e6b411816e6c82d829e4b5ef5d3da3ff2ce01e0
Namit <namityadav008@gmail.com>
2024-02-05 13:28:41 +0530

    changes in code

--- Questions.r
+++ Questions.r
@@ -1,1 +1,13 @@
-"hello"
+# question :-
+# create and arrya of 4 4 5
+# assign name to the element of an array
+#  Access al rows of matrix 1 
+# Access 2nd and 3rd column of 2nd ,matric 
+# Accesss 2nd column and 3rd row of 4th matrix
+# access 3rd column 3rd row of 3rd matrix
+# access element in the 3rd row and 4th column 1st matrix
+# use apply function ND perform sum
+# use apply function and perform sum on column
+# create two arrays and perform all arthimetic operation
+
+

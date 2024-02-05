2ab282797e668d36fc34c90f86ade6006af8c2cb
Namit <namityadav008@gmail.com>
2024-02-05 13:16:27 +0530

    changes in code

---
+++ Array.r
@@ -0,0 +1,18 @@
+a <-c(1,2,3,4,5)
+
+b<-c(5,6,7,8,9)
+
+d<- array(c(a,b) , dim=c(2,2))
+d<- array(c(a,b) , dim=c(2,2))
+
+
+# prints the matrix
+print(d)
+
+
+#prints any particular element
+
+e<- apply(d,c(1),Sum)
+
+
+

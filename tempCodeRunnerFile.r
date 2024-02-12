01ce0c52d029961723f59a18cb8d9f96b6a94f43
Namit <namityadav008@gmail.com>
2024-02-12 12:33:14 +0530

    code

--- tempCodeRunnerFile.r
+++ tempCodeRunnerFile.r
@@ -1,2 +1,3 @@
-b1<-c(1,100,10)
-print(b1)
+a<-list(c("Ankit","Mansi","Mahima","Amit")  , list("MBA","MCA","BBA","B.Tech"),c(10,8,5,7),list("Delhi","MCA","Jammu","Kshmir"))
+#array ek sath print hoga or list 1by1
+print(a[[3]])

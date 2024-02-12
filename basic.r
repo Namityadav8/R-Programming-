9ef92bc3d0ce570d01a14fa447e6884046d02dec
Namit <namityadav008@gmail.com>
2024-02-12 12:18:46 +0530

    some code added

--- basic.r
+++ basic.r
@@ -1,2 +1,3 @@
-a<-"hello kaisa h"
-a
+a<-list(c("Ankit","Mansi","Mahima","Amit")  , list("MBA","MCA","BBA","B.Tech"),c(10,8,5,7),list("Delhi","MCA","Jammu","Kshmir"))
+#array ek sath print hoga or list 1by1
+print(a)

b9cd5e6874c2ca6e881eec34e7e46b24444bab68
Namit <namityadav008@gmail.com>
2024-02-13 13:46:38 +0530

    changes in code

--- Data_Frames.r
+++ Data_Frames.r
@@ -6,3 +6,9 @@ a<-c(1,2,3,4,5,6)
 b<-c(7,8,9,10,11,12)
 result<-array(a,b,dim=3,3,2)
 result
+
+
+
+#create one matrix from 3 to 10 where nrows = 4 and byrows = false 
+
+matrix(3:10, nrow = 4, byrow = FALSE)
--- tempCodeRunnerFile.r
+++ tempCodeRunnerFile.r
@@ -1,4 +1,1 @@
-a<-c(1,2,3,4,5,6)
-b<-c(7,8,9,10,11,12)
-result<-array(a,b,dim=3,3,2)
-result
+matrix(3:10, nrow = 4, byrow = FALSE)

ad424539c793f7769d2a26258ffaaf3554cfd697
Namit <namityadav008@gmail.com>
2024-02-13 13:43:02 +0530

    data frame added in R

--- Data_Frames.r
+++ Data_Frames.r
@@ -1,2 +1,8 @@
-a<-data.frame(emp_id=c(1,2,4,6,8),emp_name=c("Sahil","Ankit","Anil","Namit"),emp_salary=c(10000,98000,110000,15000,12000),emp_address=c("Delhi","Mumbai","Rajasthan","Punjab","Goa"))
+a<-data.frame(emp_id=c(1,2,4,6,8),emp_name=c("Sahil","Ankit","Anil","Namit","Raj"),emp_salary=c(10000,98000,110000,15000,12000),emp_address=c("Delhi","Mumbai","Rajasthan","Punjab","Goa"))
 subset(a,emp_salary>10000,select=c(-emp_name,-emp_address))
+
+
+a<-c(1,2,3,4,5,6)
+b<-c(7,8,9,10,11,12)
+result<-array(a,b,dim=3,3,2)
+result

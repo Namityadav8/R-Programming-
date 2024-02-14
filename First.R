e65305686fc85377626c72dd1c36735bd7d25703
Namit <namityadav008@gmail.com>
2024-02-14 16:37:25 +0530

    Fresh start to R

---
+++ First.R
@@ -0,0 +1,7 @@
+a<- 3
+b<-5
+print(a,b)  # it will only print the first number that we have given in print
+cat(a,b)    # it will be printing everything we have given inside it 
+
+x<-10 ; y<-12         # we have to use ; for init multivar in a single line 
+print(x)

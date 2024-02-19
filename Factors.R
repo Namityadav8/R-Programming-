c4dc763d1ed93d1f468fd40d04bdbaa8374d0368
Namit <namityadav008@gmail.com>
2024-02-19 13:36:06 +0530

    added factors

---
+++ Factors.R
@@ -0,0 +1,20 @@
+#create a factor of 10 elements
+#access 8th value
+#access 6th and 9th value
+#access 1st, 2nd, 3rd, and 4th value
+#access all values except 7th value
+#access all values except 5th and 10th values
+#access all values except 6th, 7th, 8th, 9th values
+#access all values except 2nd, 4th,and 6th
+#access 2nd,5th, and 8th element by using logical values
+#modify the value of 3rd element
+#modify the value of 7th element
+#add 3 more levels
+#after adding levels modify the value of 8th element
+#after adding levels modify the value of 10th element
+#change order of levels
+#generate 4 factor levels and repeat values 5 times
+
+
+
+
--- tempCodeRunnerFile.R
+++ tempCodeRunnerFile.R
@@ -1,2 +1,24 @@
-name <- readline(prompt = "Enter your name :")
-  name
+a<-3
+b<-5
+# cat(3," is a prime number")
+text<- paste(a,b)
+print(text)
+
+
+for(i in 1:5){
+    if(i==3){
+        next
+    }  # basically next skips the desired condition while keeping on the rest loop 
+    print(i)
+}
+
+x<-1:10
+for(var in x){
+    print("Namit")
+}
+
+x<- 1:23
+for(i in 1:10){
+    print(paste("number: ",i))
+}
+

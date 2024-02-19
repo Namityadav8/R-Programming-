3b349206c42bad145e49f55870665e9393abb7fa
Namit <namityadav008@gmail.com>
2024-02-19 13:38:32 +0530

    solved some ques

--- Factors.R
+++ Factors.R
@@ -15,6 +15,53 @@
 #change order of levels
 #generate 4 factor levels and repeat values 5 times
 
+#1. 
+# Create a factor of 10 elements
+my_factor <- factor(1:10)
 
+# Access 8th value
+print(my_factor[8])
+
+# Access 6th and 9th value
+print(my_factor[c(6, 9)])
+
+# Access 1st, 2nd, 3rd, and 4th value
+print(my_factor[1:4])
+
+# Access all values except 7th value
+print(my_factor[-7])
+
+# Access all values except 5th and 10th values
+print(my_factor[-c(5, 10)])
+
+# Access all values except 6th, 7th, 8th, 9th values
+print(my_factor[-c(6:9)])
+
+# Access all values except 2nd, 4th, and 6th
+print(my_factor[-c(2, 4, 6)])
+
+# Access 2nd, 5th, and 8th element by using logical values
+print(my_factor[c(FALSE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE)])
+
+# Modify the value of 3rd element
+my_factor[3] <- "Modified"
+
+# Modify the value of 7th element
+my_factor[7] <- "Modified"
+
+# Add 3 more levels
+my_factor <- factor(c(levels(my_factor), "Level11", "Level12", "Level13"))
+
+# After adding levels modify the value of 8th element
+my_factor[8] <- "Modified"
+
+# After adding levels modify the value of 10th element
+my_factor[10] <- "Modified"
+
+# Change order of levels
+my_factor <- factor(my_factor, levels = c("Modified", "Level11", "Level12", "Level13", "Level1", "Level2", "Level3", "Level4", "Level5", "Level6", "Level7", "Level8", "Level9", "Level10"))
+
+# Generate 4 factor levels and repeat values 5 times
+my_factor <- rep(factor(1:4), each = 5)
 
 

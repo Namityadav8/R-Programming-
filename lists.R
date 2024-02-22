72848e524aaf1dbee0143c042e00686f94fa34e6
Namit <namityadav008@gmail.com>
2024-02-22 18:38:20 +0530

    list and vectors learned in the list

---
+++ lists.R
@@ -0,0 +1,10 @@
+# a<-c(9,8,3,43,35,78,2,4,21,1)
+# print(sort(a))
+
+
+
+list1<-list(c("namit","amit","pamit") , c(23,56,64), list("B.Tech","MBA","Arch"))
+
+#Naming of the vectors present in the list 
+names(list1)<-c("NAMES","MARKS","COURSES")
+print(list1)
--- tempCodeRunnerFile.R
+++ tempCodeRunnerFile.R
@@ -1,3 +1,5 @@
-a<-c("namit","yadav","hellonamit","namityadav")
-b<-"namit"
-print(grep(b,a))
+list1<-list(c("namit","amit","pamit") , c(23,56,64), list("B.Tech","MBA","Arch"))
+
+
+names(list1)<-c("NAMES","MARKS","COURSES")
+print(list1)

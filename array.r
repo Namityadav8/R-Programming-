c7e794bad1d85bbcbb756bd0a0b52ab55e54a5c3
Namit <namityadav008@gmail.com>
2024-02-24 14:57:00 +0530

    added matrix in R

--- array.r
+++ array.r
@@ -1,4 +1,9 @@
 a<-c(1,3,5,57)
 b<-c(10,20,30,40,50)
-d<-array(c(a,b) , dim=c(3,5,2))
-print(d)
+# d<-array(c(a,b) , dim=c(3,5,2))
+# print(d)
+
+a<-c(1,3,5,57)
+b<-c(10,20,30,40,50)
+x<-array(c(a,b),dim=c(5,3))  # if no dim then by default it will make one matrix 
+print(x)
--- tempCodeRunnerFile.R
+++ tempCodeRunnerFile.R
@@ -1,4 +1,4 @@
 a<-c(1,3,5,57)
 b<-c(10,20,30,40,50)
-d<-array(c(a,b) , dim=c(3,5,2))
-print(d)
+x<-array(c(a,b),dim=c(5,3))
+print(x)

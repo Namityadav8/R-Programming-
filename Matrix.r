3ad59cf4c11c34b0e1e926f3a8fd5b487def3827
Namit <namityadav008@gmail.com>
2024-02-24 22:10:03 +0530

    changes done in R

--- Matrix.r
+++ Matrix.r
@@ -1,3 +1,15 @@
 
 b<-matrix(c(1,2,3,4,5,6,7,8,9,10),nrow=4,byrow=TRUE)
 print(b)
+
+
+row_name<-c("r1","r2","r3")
+col_name<-c("c1","c2")
+x<-matrix(1:23,nrow=3,ncol=2,byrow=TRUE,dimnames=list(row_name,col_name))
+print(x)
+
+#Accessing elements in a Matrix
+row_name<-c("r1","r2","r3")
+col_name<-c("c1","c2")
+x<-matrix(1:23,nrow=3,ncol=2,byrow=TRUE,dimnames=list(row_name,col_name))
+print(x)

0b4d7f5fd60e46c597cd5773b8b591bac25fa17d
Namit <namityadav008@gmail.com>
2024-02-26 12:35:30 +0530

    done

--- Data_frames.r
+++ Data_frames.r
@@ -10,7 +10,7 @@ a <- data.frame(
   stud_name = c("namit", "aadi", "raj", "aman", "dhawan"),
   stud_roll = c(23, 25, 69, 56, 76),
   stud_marks = c(89, 98, 100, 78, 87)
-)
+) 
 
 b <- head(a, 3)
 print(b)
--- tempCodeRunnerFile.R
+++ tempCodeRunnerFile.R
@@ -1,5 +1,3 @@
-a<-data.frame(
-    id = 1:3,
-    names=c("namit","aadi","cheetah")
-)
-print(a)
+list1<-list(c("namit","amit","pamit") , c(23,56,64), list("B.Tech","MBA","Arch"))
+#how to access the elements inside the list which is having many list 
+print(list1[[3]][-2])

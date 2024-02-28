7cdd4b2740f5ea011804bd34a085513a92c33cf3
Namit <namityadav008@gmail.com>
2024-02-28 23:35:01 +0530

    some changes done in R

--- Data_frames.r
+++ Data_frames.r
@@ -32,11 +32,12 @@ Data_Frame <- data.frame (
   Pulse = c(100, 150, 120),
   Duration = c(60, 30, 45)
 )
-str(Data_Frame)
+print(Data_Frame)
 col <- data.frame(Data_Frame$Training,Data_Frame$Pulse)
 print(col)
 
 print("Now printing the rows ")
 
 x<- Data_Frame[c(1,3),]
-print(x)
+y<-Data_Frame[c(1,3),3]
+print(y)

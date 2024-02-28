170bcff8789d131e52e5307e081d9eb79f9dd41c
Namit <namityadav008@gmail.com>
2024-02-28 22:46:07 +0530

    some changes done in R

--- Data_frames.r
+++ Data_frames.r
@@ -32,6 +32,11 @@ Data_Frame <- data.frame (
   Pulse = c(100, 150, 120),
   Duration = c(60, 30, 45)
 )
+str(Data_Frame)
+col <- data.frame(Data_Frame$Training,Data_Frame$Pulse)
+print(col)
 
-# Print the data frame
-Data_Frame
+print("Now printing the rows ")
+
+x<- Data_Frame[c(1,3),]
+print(x)
--- tempCodeRunnerFile.R
+++ tempCodeRunnerFile.R
@@ -1,5 +1,13 @@
-x<-data.frame(
-  Names =c("namit","yadav","amit","pamit"),
-  Age =c(23,54,65,45)
+Data_Frame <- data.frame (
+  Training = c("Strength", "Stamina", "Other"),   # data frames will create data in form of columns 
+  Pulse = c(100, 150, 120),
+  Duration = c(60, 30, 45)
 )
-print(head(x,2))
+str(Data_Frame)
+col <- data.frame(Data_Frame$Training,Data_Frame$Pulse)
+print(col)
+
+print("Now printing the rows ")
+
+x<- Data_Frame[c(1,3),]
+print(x)

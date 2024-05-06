89c09890df524096ff3c863eae74ff16fab70109
Namit <namityadav008@gmail.com>
2024-05-07 00:40:48 +0530

    learning R

--- CA-2.2.r
+++ CA-2.2.r
@@ -7,5 +7,5 @@ c.	Arrange the dataset according the salary in descending order
 a<- " Entering the SQL file one which you want to perform the execution on "
 
 
-b<-read.csv("C:\Users\Namit Yadav\Downloads\input.csv")
-b
+b<-read.csv("C:\\Users\\Namit Yadav\\Downloads\\input.csv")
+print(b)
--- Practise.r
+++ Practise.r
@@ -125,14 +125,140 @@ a<-as.numeric(readline(prompt="Enter your number :"))
 cat("the number chosen in",a)
 
 
+a<-as.integer(readline(prompt="Enter your number : \n"))
+b<-as.integer(readline(prompt="Enter your number : \n"))
+
+if(a>b){
+    print("a is greater than b")
+}else{
+    print("b is greater than a")
+}
+
+
+ a<-function(x,y){
+   z<- x+y
+   print(z)
+ }
+
+ a(3,4)
+
+x<-3.4
+print(trunc(x))
+floor(x)
+ceiling(x)
+abs(-3)
+exp(x)
+
+a<-"namit"
+print(substr(a,1,2))
+
+
+
+
+a<-c(1,2,34,4,546,56,3)
+a<- 1:20
+
+a<-seq(1,56 , lenght.out=100)
+print(a)
+
+x<-c("namit","akm","ram","aam","dam")
+x[c(TRUE,FALSE,TRUE,FALSE,TRUE)]
+print(x[-c(1,2)])
+
+a<-c(1,2,3)
+b<-c(1,2,3)
+d<-c(a,b)
+print(d)        # Combining the vectors 
+print(unique(d)) # unique is used to remove the duplicate values .
+
+
+print(d)
+d[2]<-"45"       # updating the values of vector
+print(d)
+
+
+
+a<-rep(c(1,2,3)  , times = c(3,2,1))
+print(a)
+x<-c(1,2,34,4)
+x[x=4]<-23
+print(x)
+
+
+a<-list(c(1,2,3),c("namit","amit","pawan"))
+names(a) <- c("S.NO." , "NAME")
+print(a$NAME)
+print(a[[-(1)]][-c(1,2)])
+unlist(a)
+print(a)
+
+
+x<-c(1,2,34,4)
+any(x>0)     # These both are boolean funtions 
+all(x>3)
+
+for(i in a){
+    print(i)       # Printing the values of a list 
+}
+
+x<-c(1,2,3,4)
+a<-array(x,dim =c(3,3))
+print(a)
+
+row_name<-c("a","b","c")
+col_name<-c("d","e","f")
+
+print(a)
+
+a[2,2]<-69
+
+print(a)
+
+a[a>1]<-111
+print(a)
+
+
+print(a[1,])
 
 
 
 
 
+a<-matrix(1:12 , nrow = 4 , ncol =3 , byrow=TRUE)
+print(b)
+print(b[c(1,2),2])
+b<-matrix(13:24 , nrow = 4 , ncol =3 , byrow=TRUE)
+
+x<-rbind(a,b)
+print(x)
 
 
 
+# Sample matrices (same dimensions for cbind)
+matrix1 <- matrix(1:6, nrow = 3, ncol = 2)
+print(matrix1)
+
+a<-matrix()
+matrix2 <- matrix(7:12, nrow = 3, ncol = 2)
+
+# Combine columns using cbind
+combined_matrix <- rbind(matrix1, matrix2)
+
+# Print the combined matrix
+print(combined_matrix)
+
+
+
+
+a<-data.frame(
+    name=c("namit","amit","Pamit"),
+    age=c(23,34,45)
+)
+str(a)
+# how data frame can be accessed 
+print(a$age)
+print(a[1,])  
+print(a[c(1,2),1])
 
 
 
